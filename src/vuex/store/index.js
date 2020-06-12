7450be019287e8ac6df864c757e4cc573e538ddc
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-13 01:13:12 +0700

    [vuex]: Added auth to module

--- src/vuex/store/index.js
+++ src/vuex/store/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+// Importing module
+import auth from './modules/auth'
+
+Vue.use(Vuex) // Initial handshake
 
 export default new Vuex.Store({
-    modules: {}
+    modules: {
+        auth
+    }
 })
