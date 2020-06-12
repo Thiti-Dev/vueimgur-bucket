a412cf69f5a4494e7d15b00ef321b9205e216211
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-13 00:01:33 +0700

    :lipstick: [component]: Created AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ui inverted segment">
+    <div class="ui inverted secondary pointing menu">
+      <a class="active item">Vueimgur-Bucket</a>
+      <div class="right menu">
+        <a class="item">Sign In</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader"
+};
+</script>
