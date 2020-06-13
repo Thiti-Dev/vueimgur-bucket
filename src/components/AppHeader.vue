e4dec3285384c23f55bd400f82595a24228a396e
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-13 17:03:09 +0700

    [AppHeader-component]: Rendering whether using loggedIn or not by mapping the getters

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,17 +3,21 @@
     <div class="ui inverted secondary pointing menu">
       <a class="active item">Vueimgur-Bucket</a>
       <div class="right menu">
-        <a href="#" class="item" @click="login">Sign In</a>
+        <div v-if="isLoggedIn">
+          <a href="#" class="item">Sign Out</a>
+        </div>
+        <a v-else href="#" class="item" @click="login">Sign In</a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "AppHeader",
+  computed: mapGetters(["isLoggedIn"]),
   methods: mapActions(["login"])
 };
 </script>
