a757d3d564dda87db9226733e05582bdb4dc9c42
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-13 17:02:23 +0700

    :wavy_dash: [main-vue-app]: Initialized VueRouter, added oauth2/callback path

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
 import store from './vuex/store'
 
+// Importing components
+import AuthHandler from './components/AuthHandler'
+
+Vue.use(VueRouter)
+
+//
+// ─── ROUTE ──────────────────────────────────────────────────────────────────────
+//
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {path: '/oauth2/callback',component: AuthHandler}
+    ]
+})
+
+// ────────────────────────────────────────────────────────────────────────────────
+
+
 new Vue({
+    router,
     store, // follow up the handshake
     render: h=> h(App)
 }).$mount('#app')
