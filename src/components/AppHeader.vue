87a70e1c59438a37c5d4dabce98ee948a2ad4ec5
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-13 21:54:13 +0700

    :zap: [AppHeader-component]: Replaced anchor tag with router-link

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="ui inverted segment">
     <div class="ui inverted secondary pointing menu">
-      <a class="active item">Vueimgur-Bucket</a>
+      <router-link to="/" class="active item">Vueimgur-Bucket</router-link>
       <div class="right menu">
         <div v-if="isLoggedIn" class="horizontal">
-          <a href="#" class="item">Gallery</a>
-          <a href="#" class="item">Upload</a>
+          <router-link to="/" class="item">Gallery</router-link>
+          <router-link to="/upload" class="item">Upload</router-link>
           <a href="#" class="item" @click="logout">Sign Out</a>
         </div>
         <a v-else href="#" class="item" @click="login">Sign In</a>
