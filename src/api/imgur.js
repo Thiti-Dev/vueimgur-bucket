6279aaf97769d93bb2854f2ba3d7a7142d20ab28
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-13 22:42:40 +0700

    :zap: [api-helper]: Created fetchImages which returns a promise

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import qs from 'qs'
 
 // Importing constrant
@@ -12,5 +13,12 @@ export default {
         }
 
         window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(querystring)}` // navigating to oauth2
+    },
+    fetchImages(token){
+        return axios.get(`${ROOT_URL}/3/account/me/images`,{
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
     }
 }
