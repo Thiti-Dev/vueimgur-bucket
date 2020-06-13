ab723782b631e82fcae61548479bfb9108646e78
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-13 22:44:54 +0700

    :lipstick::sparkles: [component]: Created ImageList with a simply directive mapped from vuex

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>Image List</div>
+  <div>
+    <img v-for="(image,index) in allImage" :key="index" :src="image.link" width="150" height="150" />
+  </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
 export default {
-  name: "ImageList"
+  name: "ImageList",
+  computed: mapGetters(["allImage"]),
+  methods: mapActions(["fetchImages"]),
+  created() {
+    this.fetchImages();
+  }
 };
 </script>
