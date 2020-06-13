97ac4b36f219152a9b8d82fd07b83582d1c50eb6
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-13 22:44:10 +0700

    :heavy_plus_sign: [vuex]: Initialize images module in to the vuex.store

--- src/vuex/store/index.js
+++ src/vuex/store/index.js
@@ -3,11 +3,13 @@ import Vuex from 'vuex'
 
 // Importing module
 import auth from './modules/auth'
+import images from './modules/images'
 
 Vue.use(Vuex) // Initial handshake
 
 export default new Vuex.Store({
     modules: {
-        auth
+        auth,
+        images
     }
 })
